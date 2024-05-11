2b04e735bccc60d930a0dc6c111999f1145f2ca9
Binoy Jayan <binoyjayan@gmail.com>
2024-05-11 07:10:57 -0700

    receive data from tun nic and parse ipv4 and tcp

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # tcp-rs
 TCP/IP implementation in rust
+
+
+## Create a tun interface
+
+```
+sudo ip tuntap add mode tun user <username>
+```
+
+## Set capability
+
+```
+sudo setcap cap_net_admin=ep target/debug/tcp-rs
+sudo setcap cap_net_admin=ep target/release/tcp-rs
+```
+
+## Set ip
+
+```
+sudo ip addr add 192.168.0.22/24 dev tun0 
+sudo ip link set up dev tun0
+```
+
+## Establish a tcp connection
+
+```
+nc 192.168.0.2 80
+```
+
+## References
+
+https://www.kernel.org/doc/Documentation/networking/tuntap.txt
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("Failed to create TUN device");
+use etherparse::{IpNumber, Ipv4HeaderSlice, TcpHeaderSlice};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("Failed to create TUN device");
+    // Create a buffer. 4 bytes to hold extra tun frame format
+    let mut buf = [0u8; 1504];
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        // By default, the tun frame format includes flags,proto,raw data
+        let _flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if proto != 0x800 {
+            continue; // ignore non-ipv4
+        }
+        match Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(ip) => {
+                let src = ip.source_addr();
+                let dst = ip.destination_addr();
+                let proto = ip.protocol();
+                if proto != IpNumber::TCP {
+                    continue;                    
+                }
+                let tcp_raw = &buf[4 + ip.slice().len()..];
+                match TcpHeaderSlice::from_slice(tcp_raw) {
+                    Ok(tcp) => {
+                        let srcp = tcp.source_port();
+                        let dstp = tcp.destination_port();
+                        let len = tcp.slice().len();
+                        println!("TCP [{}:{}] {}:{} -> {}:{}", nbytes, len, src, srcp, dst, dstp);
+                    }
+                    Err(e) => {
+                        eprintln!("Ignoring packet. len:{} Err: {}", nbytes, e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Ignoring packet. len:{} Err: {}", nbytes, e);
+            }
+        }
+    }
 }
